8eb7a36f3785ca19f41839f71d5a95205bbfb7dc
Paul D'Amico <pauldamico88@gmail.com>
2023-07-23 11:34:10 -0400

    fixed post modal

--- client/src/components/allposts/UpdatePostModal.js
+++ client/src/components/allposts/UpdatePostModal.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
 import ReactPlayer from "react-player";
+import FileModel from "./FileModel";
 import ImageModal from "./ImageModal";
 
 export default function UpdatePostModel(props) {
-  console.log(props.image);
-
   const [updatePost, setUpdatedPost] = useState({
     editedPost: props.post,
     editedVideo: props.video,
   });
+  const [replaceVideo, setReplaceVideo] = useState(false);
+
+  const toggleReplaceVideo = () => {
+    setReplaceVideo(!replaceVideo);
+  };
 
   const editPostHandler = (event) => {
     const { name, value } = event.target;
@@ -34,18 +38,32 @@ export default function UpdatePostModel(props) {
         value={updatePost.editedPost}
         onChange={editPostHandler}
       />
-    {props.image ? (
-                <img height="100%" width="100%" onClick={props.openImageResize} alt="" src={`/uploads/${props.username}/postedimages/${props.image}`} />
-              ) : null}
-           
-      {props.video ? (
-        <ReactPlayer
-          style={{ cursor: "pointer" }}
-          width="100%"
+      {props.image ? (
+        <img
           height="100%"
-          url={`/uploads/${props.username}/postedvideos/${updatePost.editedVideo}`}
-          controls
+          width="100%"
+          onClick={props.openImageResize}
+          alt=""
+          src={`/uploads/${props.username}/postedimages/${props.image}`}
         />
+      ) : null}
+
+      {props.video ? (
+        <div>
+          {!replaceVideo ? (
+            <ReactPlayer
+              onClick={toggleReplaceVideo}
+              style={{ cursor: "pointer" }}
+              width="100%"
+              height="100%"
+              url={`/uploads/${props.username}/postedvideos/${updatePost.editedVideo}`}
+            />
+          ) : null}
+          {replaceVideo ? (
+       <p>Change me</p>
+          ) : null}
+          
+        </div>
       ) : null}
 
       <Button variant="contained" onClick={savePost}>
